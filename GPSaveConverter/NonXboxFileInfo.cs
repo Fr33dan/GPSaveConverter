f1aee3b533b68f92625c962471d8aa95427a5378
Fr33dan <D4rk_fr33d3n@hotmail.com>
2022-03-08 16:20:42 -0500

    Create data structure for non-xbox files.

---
+++ GPSaveConverter/NonXboxFileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPSaveConverter
+{
+    internal class NonXboxFileInfo
+    {
+        public string FilePath { get; set; }
+        public string RelativePath { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
