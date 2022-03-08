d1a552bd264a0e7a331b1e525dae670b9587d8f6
Fr33dan <D4rk_fr33d3n@hotmail.com>
2022-03-08 16:16:16 -0500

    Fix library query when package not found. Add initial games to library.

--- GPSaveConverter/GameLibrary.cs
+++ GPSaveConverter/GameLibrary.cs
@@ -9,8 +9,14 @@ namespace GPSaveConverter
 {
     internal class GameLibrary
     {
-        private Dictionary<string, string> library;
-        public void LoadCSV(string libraryFile)
+        private static Dictionary<string, string> library;
+
+        static GameLibrary()
+        {
+            LoadCSV("GameLibrary.psv");
+        }
+
+        public static void LoadCSV(string libraryFile)
         {
             library = new Dictionary<string, string>();
             string[] gameList = File.ReadAllLines(libraryFile);
@@ -21,11 +27,14 @@ namespace GPSaveConverter
             }
         }
 
-        public string getNonXboxSaveLocation(string gamePassID)
+        public static string getNonXboxSaveLocation(string gamePassID)
         {
-            string location = library[gamePassID];
-            
-            if(location == null) location = Environment.ExpandEnvironmentVariables(location);
+            string location = null;
+
+            if(library.TryGetValue(gamePassID, out location))
+            {
+                location = Environment.ExpandEnvironmentVariables(location);
+            }
 
             return location;
         }
