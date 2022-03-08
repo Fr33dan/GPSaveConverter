2e37ee9710464fc9301d05fea982c17961489ae4
Fr33dan <D4rk_fr33d3n@hotmail.com>
2022-03-08 16:21:24 -0500

    Update project files for prevous file changes.

--- GPSaveConverter/Program.cs
+++ GPSaveConverter/Program.cs
@@ -16,7 +16,7 @@ namespace GPSaveConverter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SaveFileConverterForm());
         }
     }
 }
