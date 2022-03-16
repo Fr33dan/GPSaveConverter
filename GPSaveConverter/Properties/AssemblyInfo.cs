227af9924e3816593124050795516ff37e8ab022
Fr33dan <D4rk_fr33d3n@hotmail.com>
2022-03-16 13:11:30 -0400

    Update assembly info.

--- GPSaveConverter/Properties/AssemblyInfo.cs
+++ GPSaveConverter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GPSaveConverter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Xbox Save File Converter")]
+[assembly: AssemblyDescription("Utility to transfer save files between the games installed from the Xbox app and other storefronts.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GPSaveConverter")]
+[assembly: AssemblyProduct("Xbox Save File Converter")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
