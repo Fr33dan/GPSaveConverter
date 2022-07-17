2f33de8440be22188b8a04c2767f0442d71d5d32
Fr33dan <D4rk_fr33d3n@hotmail.com>
2022-07-17 19:19:18 -0400

    Bump version number.

--- GPSaveConverter/Properties/AssemblyInfo.cs
+++ GPSaveConverter/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.4.3")]
+[assembly: AssemblyFileVersion("0.4.5.0")]
