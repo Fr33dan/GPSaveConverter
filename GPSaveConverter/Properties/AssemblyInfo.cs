d5090d3824194063698a5a99e48920531cdabad1
Fr33dan <d4rk_fr33d3n@hotmail.com>
2024-03-31 13:10:35 -0400

    Update version number and copyright date.

--- GPSaveConverter/Properties/AssemblyInfo.cs
+++ GPSaveConverter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Xbox Save File Converter")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.6.0")]
+[assembly: AssemblyFileVersion("0.4.7.0")]
